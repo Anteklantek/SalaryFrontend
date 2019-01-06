ea456737b685cf78f4c8cdcd942ddfd1002f0e84
Antek <antonik68@gmail.com>
2019-01-06 23:03:47 +0100

    extract url and parameter to global settings

--- src/app/salaries-form/salary.service.ts
+++ src/app/salaries-form/salary.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root',
@@ -8,8 +9,8 @@ export class SalaryService {
   constructor(private http: HttpClient) { }
 
   getSalaryList(dayGrossSalary : string){
-    let url = "http://localhost:8090/salary";
-    let params = new HttpParams().set('dayGrossSalary', dayGrossSalary.replace(',','.'));
+    const url = environment.salaryApiUrl;
+    const params = new HttpParams().set(environment.salaryApiDayGrossParameter, dayGrossSalary.replace(',','.'));
     return this.http.get<SalaryList>(url, {params: params})
   }
 }
